9b8a1303696d9b4468393ab1301dcbbc8b7768f5
Galois <Galoisb95@gmail.com>
2024-01-27 00:29:02 +0100

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, screen } from '@testing-library/react';
+import UseStateExamples from './examples/UseStateExamples';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('UseStateExamples Component', () => {
+  test('Le compteur démarre à 0', () => {
+    render(<UseStateExamples />);
+    const counterText = screen.getByText(/Compteur : 0/i);
+    expect(counterText).toBeInTheDocument();
+  });
+
+  test('Le compteur s\'incrémente de 1 lors du clic sur le bouton', () => {
+    render(<UseStateExamples />);
+    const button = screen.getByText(/Incrémenter le compteur et tester le useState/i);
+    fireEvent.click(button);
+    const counterText = screen.getByText(/Compteur : 1/i);
+    expect(counterText).toBeInTheDocument();
+  });
 });
