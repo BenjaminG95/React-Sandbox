7d48f6c3f175cf67a4f14dc12b793060c06b67dc
Galois <Galoisb95@gmail.com>
2024-01-27 02:48:04 +0100

    refactoring

--- src/examples/useStateExample.js
+++ src/examples/useStateExample.js
@@ -109,9 +109,9 @@ function UseStateExample() {
     }
 
     return (<>
-        {/*Changez le nom de la fonction appelée dans le onClick pour tester les différentes méthodes*/}
         <div className="flex-column">
             <h2>UseState( )</h2>
+            {/*Changez le nom de la fonction appelée dans le onClick pour tester les différentes méthodes*/}
             <button className='btn btn-primary'
                     onClick={incrementCountDirectWay}
             >
