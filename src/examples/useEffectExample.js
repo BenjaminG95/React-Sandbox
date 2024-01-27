15a4536526e990ba7b522d65b659ca0e66ab9218
Galois <Galoisb95@gmail.com>
2024-01-27 01:00:36 +0100

    add description of useEffect

--- src/examples/useEffectExample.js
+++ src/examples/useEffectExample.js
@@ -1,3 +1,39 @@
+/**
+ * useEffect est un Hook dans React qui permet d'effectuer des effets de bord (side effects) dans les composants
+ * fonctionnels. Un "effet de bord" est toute opération qui affecte autre chose que la valeur de retour d'une
+ * fonction et qui n'est pas directement liée au rendu de l'interface utilisateur, comme les requêtes de données,
+ * les manipulations directes du DOM, les abonnements, etc. useEffect est utilisé pour organiser et gérer ces
+ * opérations de manière efficace et cohérente dans les composants fonctionnels.
+ *
+ * Voici quelques points clés sur useEffect :
+ *
+ * Combinaison des Cycles de Vie :
+ * useEffect sert à combiner les capacités de plusieurs méthodes de cycle de vie des composants de classe,
+ * telles que componentDidMount, componentDidUpdate, et componentWillUnmount.
+ *
+ * Exécution Après le Rendu :
+ * Les effets définis dans useEffect sont exécutés après le rendu du composant.
+ * Cela signifie qu'ils ne bloquent pas le rendu visuel et permettent à l'interface utilisateur de rester réactive.
+ *
+ * Nettoyage :
+ * useEffect peut retourner une fonction de nettoyage.
+ * Cette fonction est appelée avant que le composant ne soit retiré de l'interface utilisateur
+ * (démonté) ou avant que l'effet ne soit réexécuté. Cela est utile pour nettoyer ou
+ * annuler des abonnements, des écouteurs d'événements, etc.
+ *
+ * Dépendances : useEffect prend un tableau de dépendances en deuxième argument.
+ * L'effet ne sera réexécuté que si les valeurs de ces dépendances ont changé entre les rendus.
+ * Si vous passez un tableau vide ([]), l'effet ne s'exécutera qu'une seule fois après le premier
+ * rendu, similaire à componentDidMount.
+ *
+ * Utilisations Courantes :
+ * Chargement de données depuis une API.
+ * Mise en place et nettoyage d'abonnements ou d'écouteurs d'événements.
+ * Mises à jour manuelles du DOM.
+ * Enregistrement et lecture de données dans le stockage local du navigateur.
+ * Règles d'Utilisation : Comme pour tous les Hooks, useEffect doit être appelé au niveau supérieur d'un composant fonctionnel ou d'un autre Hook personnalisé, et non à l'intérieur de boucles, conditions ou fonctions imbriquées.
+ */
+
 import React, {useState, useEffect} from "react";
 
 function EffectExample() {
