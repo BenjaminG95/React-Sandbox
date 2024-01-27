e5e61e1fce25931a12e6cea068a27b8b488fd0a1
Galois <Galoisb95@gmail.com>
2024-01-27 01:10:15 +0100

    add react logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import logo from './logo.svg';
+
 import UseStateExamples from './examples/useStateExamples';
 import UseEffectExample from "./examples/useEffectExample";
 
@@ -6,6 +8,7 @@ function App() {
     return (
         <div className={'App'}>
             <header className={'App-header'}>
+                <img src={logo} className="App-logo" alt="logo" />
                 <div className={'flex-container flex-evenly w-100'}>
                     <UseStateExamples/>
                     <UseEffectExample/>
