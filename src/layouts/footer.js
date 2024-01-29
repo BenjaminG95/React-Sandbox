23484aff7f31bea5b595df0dcb93ad688b0777fe
Galois <Galoisb95@gmail.com>
2024-01-29 03:44:41 +0100

    improve UI

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -4,9 +4,12 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="container">
-                <span>© 2024 React Sandbox - Sous licence MIT</span>
-                <div className="social-links">
-                    <a href="https://github.com/BenjaminG95/React-Sandbox" target="_blank" rel="noreferrer">GitHub</a>
+                <span className={'footer-text'}>© 2024 React Sandbox - Sous licence MIT</span>
+                <div className={'social-links'}>
+                    <a href="https://github.com/BenjaminG95/React-Sandbox"
+                       target="_blank"
+                       rel="noreferrer"
+                    className={''}>GitHub</a>
                 </div>
             </div>
         </footer>
