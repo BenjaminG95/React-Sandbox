95207f542c340d96120d1eb757e003be84cba27c
Galois <Galoisb95@gmail.com>
2024-01-29 02:27:56 +0100

    add github links into react router examples

--- src/pages/reactRouterExample.js
+++ src/pages/reactRouterExample.js
@@ -6,8 +6,12 @@ function ReactRouterExample() {
         <div className={'App-main'}>
             <h2>Exemple de React Router</h2>
             <p className={'text-small'}>Cette page est un exemple de la façon d'utiliser React Router dans une application React.</p>
+            <a className={'color-teal text-small my-2'}
+               href={'https://github.com/BenjaminG95/React-Sandbox/blob/main/src/pages/reactRouterExample.js'}
+               target={"_blank"}
+               rel="noreferrer">Code source de la page reactRouterExample.js</a>
             <div className="navigation-links">
-                <Link to="/" className="color-teal">Retour à l'Accueil</Link>
+                <Link to="/" className={'color-teal text-small'}>Retour à l'Accueil</Link>
             </div>
         </div>
     );
--- src/pages/reactRouterWithParamExample.js
+++ src/pages/reactRouterWithParamExample.js
@@ -9,8 +9,12 @@ function ReactRouterWithParamExample() {
         <div>
             <div className={'App-main'}>
                 <h2>Le paramètre est '{param}'</h2>
-                <div className="navigation-links">
-                    <Link to="/" className="color-teal">Retour à l'Accueil</Link>
+                <a className={'color-teal text-small my-2'}
+                   href={'https://github.com/BenjaminG95/React-Sandbox/blob/main/src/pages/reactRouterWithParamExample.js'}
+                   target={'_blank'}
+                   rel="noreferrer">Code source de la page reactRouterWithParamExample.js</a>
+                <div className={'navigation-links'}>
+                    <Link to="/" className={'color-teal text-small'}>Retour à l'Accueil</Link>
                 </div>
             </div>
         </div>
