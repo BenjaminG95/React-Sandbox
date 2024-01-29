7ab3fb7d4d9c354f5ce1bb2804e3063a14d2a8ff
Galois <Galoisb95@gmail.com>
2024-01-29 02:51:56 +0100

    init react resources

--- src/components/reactResources.js
+++ src/components/reactResources.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 function reactResources(): JSX.Element {
     return (<>
         <div className={'flex-item'}>
-            <h2>Ressources React</h2>
+            <h2 className={'my-2'}>Ressources React</h2>
             <Link className={'btn btn-dark-purple decoration-none text-small mt-2'}
                   to={'/react-resources'}>Ressources React</Link>
         </div>
