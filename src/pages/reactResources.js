f532a595da68488e0f0f737476910b23f9cba3f3
Galois <Galoisb95@gmail.com>
2024-01-29 03:26:03 +0100

    add some resources

--- src/pages/reactResources.js
+++ src/pages/reactResources.js
@@ -1,16 +1,42 @@
-import {Link} from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-// Exemple de composant React Router
-function ReactRouterExample() {
-    return (<>
+const resources = [
+    {"name": "React official documentation", "url": "https://react.dev/learn"},
+    {"name": "React.js cheatsheet", "url": "https://devhints.io/react"},
+    {"name": "Différents hook prêt à l'emploi", "url": "https://usehooks.com/"},
+    {"name": "Documentation Redux", "url": "https://redux.js.org/introduction/getting-started"},
+    {"name": "Librairie Material UI", "url": "https://mui.com/material-ui/getting-started/"},
+    {"name": "Librairie Ant Design", "url": "https://ant.design/docs/react/getting-started"},
+];
+
+// TODO add more resources.
+
+function ReactResources() {
+    const resourcesHtml = resources.map(resource => (
+        <li key={resource.name}>
+            <a href={resource.url}
+               target="_blank"
+               rel="noopener noreferrer"
+               className={'color-teal d-flex'}>
+                {resource.name}
+            </a>
+        </li>
+    ));
+
+    return (
         <div className={'App-main'}>
             <h2>Ressources React</h2>
-            <p className={'text-small m'}>Comming soon.</p>
+            <div>
+                <ul className={'decoration-none'}>
+                    {resourcesHtml}
+                </ul>
+            </div>
+
             <div className="navigation-links mt-2">
                 <Link to="/" className={'color-teal text-small'}>Retour à l'Accueil</Link>
             </div>
         </div>
-    </>);
+    );
 }
-
-export default ReactRouterExample;
+export default ReactResources;
