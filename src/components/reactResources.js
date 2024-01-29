73e086dc32992f8ad2157ab5546b051ad40811e6
Galois <Galoisb95@gmail.com>
2024-01-29 04:00:11 +0100

    remove text small in resource's button

--- src/components/reactResources.js
+++ src/components/reactResources.js
@@ -4,7 +4,7 @@ function reactResources(): JSX.Element {
     return (<>
         <div className={'flex-item'}>
             <h2 className={'my-2'}>Ressources React</h2>
-            <Link className={'btn btn-dark-purple decoration-none text-small mt-2'}
+            <Link className={'btn btn-dark-purple decoration-none mt-2'}
                   to={'/react-resources'}>Ressources React</Link>
             <p className={'text-small mt-4'}>Quelques ressources liées à React!</p>
         </div>
