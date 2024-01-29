499200fe7988684e5710239c5844822c5a485296
Galois <Galoisb95@gmail.com>
2024-01-29 03:58:53 +0100

    fix github link

--- src/components/examples/useState/useStateExample.js
+++ src/components/examples/useState/useStateExample.js
@@ -118,7 +118,7 @@ function UseStateExample(): JSX.Element {
                Utiliser useState( )
             </button>
             <a className={'color-teal text-small mt-2'}
-               href={'https://github.com/BenjaminG95/React-Sandbox/blob/main/src/components/examples/useState/UseStateExample.test.js'}
+               href={'https://github.com/BenjaminG95/React-Sandbox/blob/main/src/components/examples/useState/UseStateExample.js'}
                target={"_blank"} rel="noreferrer">Code source de useStateExample</a>
             <p className={'text-small'}>Chaque clique incrémente le compteur <br/>
            Compteur actuel : {count} <br/>
