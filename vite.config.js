bb021f06bbdd091beb839d948ce8438f95ab9969
BenjaminG95 <galoisb95@gmail.com>
2024-01-30 09:54:35 +0100

    quick fix: port 3000 does not work in the development environment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
     return {
         base: '/React-Sandbox/',
-        port: 3000,
+        server: {
+            port: 3000,
+        },
         build: {
             outDir: 'build',
         },
-
         plugins: [react()],
     };
 });
